d3d144cdb15bc399b311496660fb224548ff176f
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-07-11 17:20:52 +0530

    Added: Email validation method

--- src/com/bridgelabz/EmailValidateImpl.java
+++ src/com/bridgelabz/EmailValidateImpl.java
@@ -8,7 +8,19 @@ public class EmailValidateImpl implements EmailId {
 
 	@Override
 	public void emailValidate() {
-		
+		Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Enter your Email id");
+        String email = sc.next();
+
+        Pattern pattern = Pattern.compile("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$");
+        Matcher matcher = pattern.matcher(email);
+        if(matcher.matches()){
+            System.out.println("Given email id is valid");
+        }else{
+            System.out.println("Given email id is not valid");
+        }
 		
 	}
 
