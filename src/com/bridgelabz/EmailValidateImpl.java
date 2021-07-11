a9c342806c9f16ac2d5c8eccc190adb988a076eb
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-07-11 17:18:31 +0530

    Inital Status

---
+++ src/com/bridgelabz/EmailValidateImpl.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailValidateImpl implements EmailId {
+
+	@Override
+	public void emailValidate() {
+		
+		
+	}
+
+}
---
+++ src/com/bridgelabz/EmailValidation.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class EmailValidation {
+
+	public static void main(String[] args) {
+		EmailValidateImpl eMail = new EmailValidateImpl();
+		eMail.emailValidate();
+	}
+
+}
